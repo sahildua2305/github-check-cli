30e266c5c37b3eeb00fca33085e432edc91a04e1
Sahil Dua <sahildua2305@gmail.com>
2016-06-25 00:58:21 +0530

    Basic implementation of get user info

--- cli.js
+++ cli.js
@@ -4,21 +4,47 @@
 * @Author: sahildua2305
 * @Date:   2016-06-24 20:20:19
 * @Last Modified by:   Sahil Dua
-* @Last Modified time: 2016-06-24 20:31:40
+* @Last Modified time: 2016-06-25 00:51:27
 */
 
-'use strict';
+'use strict'
 
 const yargs = require('yargs')
+const request = require('request')
+const ora = require('ora')
+
+const USER_URL = 'https://api.github.com/users/'
 
 const argv = yargs
-  .usage('$0 <cmd> [args]')
-  .option('name', {
-    alias: 'n',
-    describe: 'provide your GitHub username'
-  })
-  .command('hello', 'welcome', {}, function (argv) {
-    console.log('hello', argv.name, 'Welcome')
+  .usage('Usage: $0 <cmd> [args]')
+  .command('user', 'welcome', function (yargs) {
+    const argv = yargs
+      .usage('Usage: $0 user <github_username>')
+      .demand(2)
+      .example('$0 user sahildua2305')
+      .argv
+
+    var username = argv._[1]
+    const spinner = ora('Accessing GitHub').start()
+
+    var options = {
+      url: USER_URL + username,
+      headers: {
+        'User-Agent': 'User-' + username
+      }
+    }
+
+    request.get(options, function (err, response) {
+      spinner.stop()
+      if (err) {
+        console.log('Error!')
+      }
+      else {
+        console.log(response.body)
+      }
+    })
   })
   .help('help')
+  .alias('h', 'help')
+  .epilog('Copyrights Sahil Dua (sahildua2305) 2016')
   .argv
