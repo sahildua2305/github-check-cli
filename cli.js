3201ac33c99476fa263537654306acfaf541b074
Sahil Dua <sahildua2305@gmail.com>
2016-06-25 03:57:48 +0530

    Added repo info fetch

--- cli.js
+++ cli.js
@@ -4,7 +4,7 @@
 * @Author: sahildua2305
 * @Date:   2016-06-24 20:20:19
 * @Last Modified by:   Sahil Dua
-* @Last Modified time: 2016-06-25 02:57:54
+* @Last Modified time: 2016-06-25 03:54:38
 */
 
 'use strict'
@@ -15,15 +15,30 @@ const ora = require('ora')
 const Table = require('cli-table')
 const chalk = require('chalk')
 
-const USER_URL = 'https://api.github.com/users/'
+const BASE_URL = 'https://api.github.com/'
 
-const userNotFound = () => {
-  console.log(chalk.red.bold('404 Not Found! Please enter a valid GitHub username.'))
+const check403 = (response, body) => {
+  if (response.caseless.get('status') == '403 Forbidden') {
+    var msg = body.message.split('(')[0]
+    apiLimitCrossed(msg, response.caseless.get('x-ratelimit-reset'))
+  }
+}
+
+const check404 = (response, repo) => {
+  if (response.caseless.get('status') == '404 Not Found') {
+    notFound(repo)
+  }
+}
+
+const notFound = (repo) => {
+  var msg = '404 Not Found!'
+  msg += repo ? ' Please enter a valid combination of GitHub username and repo name.' : ' Please enter a valid GitHub username.';
+  console.log(chalk.red.bold(msg))
   reportIssue()
 }
 
 const reportIssue = () => {
-  console.log(chalk.yellow('If you think something is wrong, please open an issue at https://github.com/sahildua2305/github-check-cli/issues'))
+  console.log(chalk.yellow('If you think there is a bug, please open an issue at https://github.com/sahildua2305/github-check-cli/issues'))
   process.exit(-1)
 }
 
@@ -50,7 +65,7 @@ const argv = yargs
     const spinner = ora('Accessing GitHub').start()
 
     var options = {
-      url: USER_URL + username,
+      url: BASE_URL + 'users/' + username,
       headers: {
         'User-Agent': 'User-' + username
       }
@@ -65,17 +80,10 @@ const argv = yargs
         // console.log(response.body)
         console.log(response.caseless.get('x-ratelimit-remaining'))
 
-        if (response.caseless.get('status') == '404 Not Found') {
-          userNotFound()
-        }
-
         const userInfo = JSON.parse(response.body)
 
-        if (response.caseless.get('status') == '403 Forbidden') {
-          var msg = userInfo.message.split('(')[0]
-          apiLimitCrossed(msg, response.caseless.get('x-ratelimit-reset'))
-        }
-
+        check403(response, userInfo)
+        check404(response, false)
 
         var userInfoTable = new Table({
           head: ['Followers', 'Public Repos', 'Following'],
@@ -97,6 +105,66 @@ const argv = yargs
         if (userInfo.html_url)
           console.log(chalk.cyan('GitHub Profile Link: ') + userInfo.html_url)
         console.log(userInfoTable.toString())
+
+        contributeLink()
+        process.exit(-1)
+      }
+    })
+  })
+.command('repo', 'Check GitHub information for any repo', function (yargs) {
+    const argv = yargs
+      .usage('Usage: $0 repo <repo_owner_username> <repo_name>')
+      .demand(3)
+      .example('$0 repo sahildua2305 github-check-cli')
+      .argv
+
+    var username = argv._[1]
+    var reponame = argv._[2]
+    const spinner = ora('Fetching information from GitHub').start()
+
+    var options = {
+      url: BASE_URL + 'repos/' + username + '/' + reponame,
+      headers: {
+        'User-Agent': 'User/Repo-' + username + '/' + reponame
+      }
+    }
+
+    request.get(options, function (err, response) {
+      spinner.stop()
+      if (err) {
+        console.log('Error!')
+      }
+      else {
+        console.log(response.body)
+        console.log(response.caseless.get('x-ratelimit-remaining'))
+
+        const repoInfo = JSON.parse(response.body)
+
+        check403(response, repoInfo)
+        check404(response, true)
+
+        var repoInfoTable = new Table({
+          head: ['Watching', 'Stars', 'Forks', 'Issues'],
+          colWidths: [15, 15, 15, 15],
+          style: {
+            head: ['cyan']
+          }
+        })
+        repoInfoTable.push(
+          [repoInfo.subscribers_count, repoInfo.stargazers_count, repoInfo.forks_count, repoInfo.open_issues_count]
+        )
+
+        if (repoInfo.name)
+          console.log(chalk.cyan('Name: ') + repoInfo.name)
+        if (repoInfo.full_name)
+          console.log(chalk.cyan('Full Name: ') + repoInfo.full_name)
+        if (repoInfo.description)
+          console.log(chalk.cyan('Description: ') + repoInfo.description)
+        if (repoInfo.language)
+          console.log(chalk.cyan('Language: ') + repoInfo.language)
+        if (repoInfo.html_url)
+          console.log(chalk.cyan('GitHub Link: ') + repoInfo.html_url)
+        console.log(repoInfoTable.toString())
 
         contributeLink()
         process.exit(-1)
